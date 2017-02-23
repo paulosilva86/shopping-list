0f381dc47f6b42a55e940ddd6aeda18820e06423
Paulo Silva <paulosilva86@gmail.com>
2017-02-23 23:53:59 +0000

    Display static products
    Why:
    
    * Layout the components to see how the page looks like.
    
    This change addresses the need by:
    
    * Creating a hierarchy of components:
     -> ShoppingList
       - AddProductBar
       - ProductTable
        - ProductRow

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const PRODUCTS = [
+  { name: "Bread", quantity: 2 },
+  { name: "Milk", quantity: 1 },
+  { name: "Eggs", quantity: 6 },
+  { name: "Cookies", quantity: 3 },
+  { name: "Iogurtes", quantity: 5 }
+]
+
 class App extends Component {
   render() {
     return (
@@ -11,9 +19,68 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <ShoppingList products={PRODUCTS} />
         </p>
       </div>
+    );
+  }
+}
+
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div>
+        <AddProductBar />
+        <ProductTable products={this.props.products} />
+      </div>
+    );
+  }
+}
+
+class AddProductBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <label for="product">Product: </label>
+        <input type="text" name="product" />
+        <label for="quantity">Quantity: </label>
+        <input type="text" name="quantity" />
+        <button>Add</button>
+      </div>
+    );
+  }
+}
+
+class ProductTable extends React.Component {
+  render() {
+    var rows = [];
+    this.props.products.forEach((product) => rows.push(<ProductRow name={product.name} quantity={product.quantity} />));
+
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>Quantity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {rows}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+class ProductRow extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.name}</td>
+        <td>{this.props.quantity}</td>
+      </tr>
     );
   }
 }
