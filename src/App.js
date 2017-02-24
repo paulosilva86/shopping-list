730ba5b772ad82927a86e12710c8c0d49dcaf4b1
Paulo Silva <paulosilva86@gmail.com>
2017-02-24 14:38:49 +0000

    Add product to shopping list
    This change addresses the need by:
    
    * Adding the product and quantity to the shopping list.
    * Clears the product and quantity inputs.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const PRODUCTS = [
-  { name: "Bread", quantity: 2 },
-  { name: "Milk", quantity: 1 },
-  { name: "Eggs", quantity: 6 },
-  { name: "Cookies", quantity: 3 },
-  { name: "Iogurtes", quantity: 5 }
-]
-
 class App extends Component {
   render() {
     return (
@@ -19,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          <ShoppingList products={PRODUCTS} />
+          <ShoppingList />
         </p>
       </div>
     );
@@ -27,25 +19,83 @@ class App extends Component {
 }
 
 class ShoppingList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: '',
+      quantity: '',
+      productList: [] // { name: "Milk", quantity: "1" }
+    };
+
+    this.handleProductInput = this.handleProductInput.bind(this);
+    this.handleQuantityInput = this.handleQuantityInput.bind(this);
+    this.handleAddButtonClick = this.handleAddButtonClick.bind(this);
+  }
+
+  handleProductInput(productName) {
+    this.setState({
+      product: productName
+    })
+  }
+
+  handleQuantityInput(quantity) {
+    this.setState({
+      quantity: quantity
+    })
+  }
+
+  handleAddButtonClick() {
+    this.setState({productList: [...this.state.productList, { name: this.state.product, quantity: this.state.quantity }]});
+    this.setState({product: ''});
+    this.setState({quantity: ''});
+  }
+
   render() {
     return (
       <div>
-        <AddProductBar />
-        <ProductTable products={this.props.products} />
+        <AddProductBar
+          product={this.state.product}
+          quantity={this.state.quantity}
+          onProductInput={this.handleProductInput}
+          onQuantityInput={this.handleQuantityInput}
+          onAddButtonClick={this.handleAddButtonClick}
+        />
+        <ProductTable
+          products={this.state.productList}
+        />
       </div>
     );
   }
 }
 
 class AddProductBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleProductInputChange = this.handleProductInputChange.bind(this);
+    this.handleQuantityInputChange = this.handleQuantityInputChange.bind(this);
+    this.handleAddButtonClick = this.handleAddButtonClick.bind(this);
+  }
+
+  handleProductInputChange(e) {
+    this.props.onProductInput(e.target.value);
+  }
+
+  handleQuantityInputChange(e) {
+    this.props.onQuantityInput(e.target.value);
+  }
+
+  handleAddButtonClick(e) {
+    this.props.onAddButtonClick(e);
+  }
+
   render() {
     return (
       <div>
         <label for="product">Product: </label>
-        <input type="text" name="product" />
+        <input type="text" name="product" value={this.props.product} onChange={this.handleProductInputChange} />
         <label for="quantity">Quantity: </label>
-        <input type="text" name="quantity" />
-        <button>Add</button>
+        <input type="text" name="quantity" value={this.props.quantity} onChange={this.handleQuantityInputChange} />
+        <button onClick={this.handleAddButtonClick}>Add Product</button>
       </div>
     );
   }
